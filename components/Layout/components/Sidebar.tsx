128e0293842fb50a119c032b16150b5dcc108517
陆文杰 <luwenjiechn@qq.com>
2021-07-08 15:56:53 +0800

    fix: 删除无用代码

--- components/Layout/components/Sidebar.tsx
+++ components/Layout/components/Sidebar.tsx
@@ -1,21 +1,7 @@
-import { Menu, MenuItem, Divider } from "@blueprintjs/core";
-
 import { useToggle } from "ahooks";
 
 const Sidebar = () => {
-  const [open, { toggle }] = useToggle(false);
-  return (
-    <Menu>
-      <MenuItem icon="new-text-box" text="New text box" />
-      <MenuItem icon="new-object" text="New object" />
-      <MenuItem icon="new-link" text="New link" />
-      <Divider></Divider>
-      <MenuItem text="Settings..." icon="cog">
-        <MenuItem icon="tick" text="Save on edit" />
-        <MenuItem icon="blank" text="Compile on edit" />
-      </MenuItem>
-    </Menu>
-  );
+  return <div></div>;
 };
 
 export default Sidebar;
