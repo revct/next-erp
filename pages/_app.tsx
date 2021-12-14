ca8f6670f639c097a1d27ac0bf5b72a3e5f44379
grey.lu <grey.lu@yunlsp.com>
2021-12-14 16:35:12 +0800

    chore: 升级依赖

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import { Provider } from "next-auth/client";
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Head>
         <title>ERP系统</title>
       </Head>
       <Component {...pageProps} />
       <Script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></Script>
-    </Provider>
+    </SessionProvider>
   );
 }
 export default MyApp;
